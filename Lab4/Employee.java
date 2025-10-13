3d6a1413e482d926f99ca689b95f49e94c27b8cb
JavierReyes1 <c00312829@setu.ie>
2025-10-13 22:08:20 +0100

    Add files via upload

---
+++ Lab4/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+	private int employeeNumber;
+	private String employeeType;
+	// private Address address;
+	// private Office [] offices;
+	// private Car companyCar;
+}// end employee class

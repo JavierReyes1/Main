1c091f5109dfd95b3537b7a0c574377da6bcda69
JavierReyes1 <c00312829@setu.ie>
2025-10-16 23:11:02 +0100

    Delete Lab4 directory
    Deleting Lab4 directory to upload the finished version

--- Lab4/Staff.java
+++
@@ -1,9 +0,0 @@
-public class Staff extends Employee {
-	public Staff(Address address, Office office, String employeeType) {
-		super(address, office, "Staff");
-	}
-
-	public String getEmployeeType() {
-		return employeeType;
-	}
-}

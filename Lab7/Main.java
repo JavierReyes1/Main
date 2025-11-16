9558ff2781cb3ab627095c7189722ab6f1dfe453
JavierReyes1 <c00312829@setu.ie>
2025-11-16 11:28:43 +0000

    Enhance comments for clarity on polymorphism
    Updated comments to clarify polymorphism and garbage collection.

--- Lab7/Main.java
+++ Lab7/Main.java
@@ -1,11 +1,11 @@
 public class Main{
 	public static void main(String[] args) {
 		Vet vet = new Vet("Javier");
-		Animal cat = new Cat("Fluffy");
+		Animal cat = new Cat("Fluffy");		//Polymorphism usage. cat can use "Animal" methods, but if "Cat" object has i.e purr() method, reference variable "cat" won't be able to call it
 		Animal dog = new Dog("Milo");
 		vet.vaccinate(cat);
 		vet.vaccinate(dog);
-		cat = null;
+		cat = null;		//reference variable "cat" to object "Cat", now points "null", this object is ready for GB
 		dog = null;
 		vet = null;
 		

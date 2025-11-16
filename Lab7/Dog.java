02bbb88fb8816fed39d7b9ce77ff04b506314734
JavierReyes1 <c00312829@setu.ie>
2025-11-16 11:36:20 +0000

    Implement makeNoise method in Dog class comment

--- Lab7/Dog.java
+++ Lab7/Dog.java
@@ -2,7 +2,7 @@ public class Dog extends Animal{
 	public Dog(String name){
 		super(name, "Dog");
 	}
-	public void makeNoise(){
+	public void makeNoise(){			//abstract makeNoise(){}; was inheritaed from Animal, so it MUST be implemented here 
 		System.out.println("Ruff, Ruff");
 	}
 }

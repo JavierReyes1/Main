8f0c0cc84c258c6467a3a88dc1cc971a97889f68
JavierReyes1 <c00312829@setu.ie>
2025-12-03 14:39:10 +0000

    I thought I had uploaded the remaining classes through git but I had them in a different git branch and I never merged such branch, so now I'm uploading Lab8/*.java files to the main branch

--- Lab8/ThreeDShape.java
+++ Lab8/ThreeDShape.java
@@ -1,8 +1,20 @@
 public abstract class ThreeDShape extends Shape
 {
-	public ThreeDShape(String name, String colour)
+	private double length;
+	private double height;
+	private double base;
+	public ThreeDshape(double length, double height, double base){
+	this("ThreeDShape", "White");
+	this.lenght = lenght;
+	this.height = height;
+	this.base = base;	
+	}
+	public ThreeDShape(String name, String colour, double length, double height, double base)
   	{ 
   		super(name, colour); 
+		this.length = lenght;
+		this.height = height;
+		this.base = base;
   	}
  
   	public abstract double volume();
